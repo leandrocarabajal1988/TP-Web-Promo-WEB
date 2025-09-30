6645f8be4e37bdfa68e00cbb22bd36b3b38b06e8
leandrocarabajal1988 <leandro_carabajal@yahoo.com.ar>
2025-09-30 09:46:41 -0300

    +Negocio (+VoucherNegocio)

---
+++ Negocio/VoucherNegocio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TP_Promo_Web.Datos;
+
+namespace TP_Promo_Web.Negocio
+{
+    public class VoucherNegocio
+    {
+        public bool ValidarCodigo(string codigo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setConsulta("SELECT IdCliente FROM Vouchers WHERE CodigoVoucher = @codigo");
+                datos.setearParametro("@codigo", codigo);
+                datos.ejecutarLectura();
+
+                if (datos.Lector.Read())
+                {
+                    object idCliente = datos.Lector["IdCliente"];
+                    datos.cerrarConexion();
+                    return idCliente == DBNull.Value; // si no hay cliente asignado, se puede usar
+                }
+
+                datos.cerrarConexion();
+                return false; // No existe el código
+            }
+            catch (Exception ex)
+            {
+                datos.cerrarConexion();
+                throw ex;
+            }
+        }
+    }
+}
